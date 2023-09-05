22988323b5701fb288d4bbc719fc74937ac2619e
gxz <guoxianzhe@agora.io>
2023-09-05 14:54:37 +0800

    feat: add default export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
+import { createFakeAgoraRTC } from "./top";
+import { FakeCameraVideoTrack, FakeMicrophoneAudioTrack } from "./tracks";
+
 export * from "./tracks";
 export * from "./eventemitter";
 export * from "./client";
 export * from "./version";
+
+export default createFakeAgoraRTC({
+  setAppType(): void {
+    //
+  },
+  createMicrophoneAudioTrack: async () => FakeMicrophoneAudioTrack.create(),
+  createCameraVideoTrack: async () => FakeCameraVideoTrack.create(),
+});
