58990aea22d7e856c7358218758f5bdc4471aa0b
gxz <guoxianzhe@agora.io>
2023-10-16 14:08:58 +0800

    chore:  override AgoraRTC

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import type {
+  IBufferSourceAudioTrack,
+  ILocalAudioTrack,
+  ILocalVideoTrack,
+  ScreenVideoTrackInitConfig,
+} from "agora-rtc-sdk-ng";
+
 import { FakeRTCClient } from "./client";
 import { createFakeAgoraRTC } from "./top";
 import { FakeCameraVideoTrack, FakeMicrophoneAudioTrack } from "./tracks";
@@ -11,8 +18,49 @@ const FakeAgoraRTC = createFakeAgoraRTC({
   setAppType(): void {
     //
   },
+  setArea(): void {
+    //
+  },
+  checkSystemRequirements(): boolean {
+    return true;
+  },
+  setLogLevel(): void {
+    //
+  },
+  getCameras(): Promise<MediaDeviceInfo[]> {
+    //todo
+    return Promise.resolve([]);
+  },
+  getDevices(): Promise<MediaDeviceInfo[]> {
+    //todo
+    return Promise.resolve([]);
+  },
   createMicrophoneAudioTrack: async () => FakeMicrophoneAudioTrack.create(),
   createCameraVideoTrack: async () => FakeCameraVideoTrack.create(),
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-ignore
+  createScreenVideoTrack(
+    config: ScreenVideoTrackInitConfig,
+    withAudio?: "enable" | "disable" | "auto",
+  ): Promise<[ILocalVideoTrack, ILocalAudioTrack] | ILocalVideoTrack> {
+    // 根据参数返回视频轨 或 [视频轨,音频轨]
+    if (withAudio === "enable") {
+      return Promise.resolve([
+        FakeCameraVideoTrack.create() as ILocalVideoTrack,
+        FakeMicrophoneAudioTrack.create() as ILocalAudioTrack,
+      ]);
+    } else if (withAudio === "disable") {
+      return Promise.resolve(FakeCameraVideoTrack.create() as ILocalVideoTrack);
+    } else {
+      return Promise.resolve([
+        FakeCameraVideoTrack.create() as ILocalVideoTrack,
+        FakeMicrophoneAudioTrack.create() as ILocalAudioTrack,
+      ]);
+    }
+  },
+  createBufferSourceAudioTrack: async () =>
+    FakeMicrophoneAudioTrack.create() as unknown as IBufferSourceAudioTrack,
+  createCustomVideoTrack: () => FakeCameraVideoTrack.create(),
   createClient: () => FakeRTCClient.create(),
 });
 
