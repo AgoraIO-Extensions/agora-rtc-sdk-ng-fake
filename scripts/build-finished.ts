587fc8280abd5a5f3feb753cc8109727fb18ad2c
gxz <guoxianzhe@agora.io>
2023-10-19 11:20:06 +0800

    chore: ci

--- scripts/build-finished.ts
+++ scripts/build-finished.ts
@@ -7,7 +7,7 @@ const distDir = "dist";
 
 const packageData = {
   name: "agora-rtc-sdk-ng-fake",
-  version: "1.0.0",
+  version: `${pkg.version}`,
   description: "Fake Agora Web RTC for testing",
   main: `${pkg.name}_${pkg.version}.js`,
 };
